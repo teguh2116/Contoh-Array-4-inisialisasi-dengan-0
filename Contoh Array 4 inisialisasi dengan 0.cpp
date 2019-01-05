85902b823c192c11d92b29d10324d9cc5c8d8a11
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:33:46 +0700

    Add files via upload

---
+++ Contoh Array 4 inisialisasi dengan 0.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int bil[7] = {0}; //inisialisasi 0
+    for ( int i=0;i<7;i++)
+    {
+        printf("Elemen ke-%i = %d\n", i,bil[1]);
+    }
+    getch();
+    return 0;
+}
